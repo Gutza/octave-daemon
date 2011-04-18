a70c2be27e12006060029247a342f2b725b37f45
Bogdan Stancescu <bogdan@moongate.ro>
2011-04-18 13:40:05 +0200

    New directory structure

--- gendoc.sh
+++ gendoc.sh
@@ -1,2 +1,2 @@
 rm -rf /var/www/html/projects.moongate.ro/octave-daemon/* &&
-phpdoc -q -i /test/*,/examples/* -d . -t /var/www/html/projects.moongate.ro/octave-daemon -ti "Octave-daemon"
+phpdoc -q -i /server/test/*,/server/examples/* -d . -t /var/www/html/projects.moongate.ro/octave-daemon -ti "Octave-daemon"
