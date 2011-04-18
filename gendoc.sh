b2c125ff3dc51a0af910302fd1f88ae484f36df7
Bogdan Stancescu <bogdan@moongate.ro>
2011-04-18 15:02:43 +0200

    Ugh, you should be quiet

--- gendoc.sh
+++ gendoc.sh
@@ -1,2 +1,2 @@
 rm -rf /var/www/html/projects.moongate.ro/octave-daemon/* &&
-phpdoc -i /server/test/*,/server/examples/* -d . -t /var/www/html/projects.moongate.ro/octave-daemon -ti "Octave Daemon"
+phpdoc -q -i /server/test/*,/server/examples/* -d . -t /var/www/html/projects.moongate.ro/octave-daemon -ti "Octave Daemon"
