ce55385f6270d42c410d5dfe36de404b1410b1c8
Bogdan Stancescu <bogdan@moongate.ro>
2011-04-20 01:18:12 +0200

    Erratic and redundant

--- server/test/test.php
+++ server/test/test.php
@@ -3,51 +3,15 @@
 require dirname(dirname(__FILE__))."/include/classes/Octave_controller.php";
 
 $c=new Octave_controller();
+$c->allowPartial=true;
 $c->init();
 
-$pid=pcntl_fork();
-if ($pid==-1) {
-	die("Could not fork!");
-} elseif ($pid) {
-	// wait for child
-	pcntl_wait($status);
-	echo "Child is done\n";
-} else {
-	// The child
-	$c->hangingProcess=true;
+$result=$c->query("eye(100)");
+echo $result." -- ";
+while($c->partialResult) {
+	usleep(500);
+	$result=$c->more();
+	echo $result['stdout']." == ";
 }
 
-echo "5+5=".$c->query("5+5")."\n";
-
-//echo "asd=".$c->query("asd");
-
-echo "\n------HERE--------\n";
-
-flush();
-exit;
-echo $c->query("100-33");
-
-echo "----LATER---\n";
-
-$c->run("
-function answer = lg_factorial6( n )
-
-    answer = 1;
-    
-    if( n == 0 )
-        return;
-    else
-        for i = 2:n
-            answer = answer * i;
-        endfor
-    endif
-
-endfunction
-");
-
-echo $c->runRead("tic(); for i=1:10000 lg_factorial6( 10 ); end; toc()");
-
-echo "Finally...\n";
-echo count(explode("\n",$c->query("rand(3,1)")))."\n";
-
-echo "\n--END--\n";
+echo "Octave version: {{".$c->octave_version."}}\n";
