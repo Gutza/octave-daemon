aee523d463d8eb51e66dc89c274d73045c15f8a9
Bogdan Stancescu <bogdan@moongate.ro>
2011-05-03 14:23:46 +0200

    No frames converter

--- gendoc.sh
+++ gendoc.sh
@@ -1,2 +1,2 @@
 rm -rf /var/www/html/projects.moongate.ro/octave-daemon/* &&
-phpdoc -q -i /test/*,/server/examples/* -d . -t /var/www/html/projects.moongate.ro/octave-daemon -ti "Octave Daemon"
+phpdoc -o HTML:Smarty:PHP -q -i /test/*,/install/install-*,/server/examples/* -d . -t /var/www/html/projects.moongate.ro/octave-daemon -ti "Octave Daemon"
