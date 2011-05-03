3411b7dcc80cafc317cb8d0268ee8333dcea5f97
Bogdan Stancescu <bogdan@moongate.ro>
2011-05-03 14:24:08 +0200

    License; docu cat

--- include/classes/Octave_IP_processor.php
+++ include/classes/Octave_IP_processor.php
@@ -1,5 +1,36 @@
 <?php
+/**
+* Octave-daemon -- a network daemon for Octave, written in PHP
+*
+* Copyright (C) 2011 Bogdan Stancescu <bogdan@moongate.ro>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see {@link http://www.gnu.org/licenses/}.
+*
+* @package octave-daemon
+* @subpackage server
+* @author Bogdan Stăncescu <bogdan@moongate.ro>
+* @version 1.0
+* @copyright Copyright (c) 2011, Bogdan Stăncescu
+* @license http://www.gnu.org/licenses/agpl-3.0.html GNU Affero GPL
+*/
 
+/**
+* A small class which tests IP addresses.
+*
+* @package octave-daemon
+* @subpackage server
+*/
 class Octave_IP_processor
 {
 
