1409e5754122ead16314d84560140a5d3cdac482
Bogdan Stancescu <bogdan@moongate.ro>
2011-05-05 15:01:16 +0200

    Locking and unlocking, because otherwise it gets mangled because of parallel processing

--- test/Octave_client_Test.php
+++ test/Octave_client_Test.php
@@ -5,6 +5,7 @@ require_once "common_phpunit_test.php";
 class clientTest extends commonTests
 {
 	public static $octave=NULL;
+	private $fp;
 
 	function __construct()
 	{
@@ -12,5 +13,16 @@ class clientTest extends commonTests
 			return;
 
 		self::$octave=new Octave("localhost");
+	}
+
+	function lock()
+	{
+		$this->fp=fopen(__FILE__,'r');
+		flock($this->fp,LOCK_EX);
+	}
+
+	function unlock()
+	{
+		flock($this->fp,LOCK_UN);
 	}
 }
