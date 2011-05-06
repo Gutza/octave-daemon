997c0e963092bbd2bd95f76005719b68b5cf0b11
Bogdan Stancescu <bogdan@moongate.ro>
2011-05-06 18:37:08 +0200

    Doc updates

--- gendoc.sh
+++ gendoc.sh
@@ -1,2 +1,2 @@
 rm -rf /var/www/html/projects.moongate.ro/octave-daemon/* &&
-phpdoc -o HTML:Smarty:PHP -q -i /test/*,/install/install-*,/server/examples/* -d . -t /var/www/html/projects.moongate.ro/octave-daemon -ti "Octave Daemon"
+phpdoc -o HTML:Smarty:PHP -q -i /examples/*,/test/*,/install/install-* -d . -t /var/www/html/projects.moongate.ro/octave-daemon -ti "Octave Daemon"
