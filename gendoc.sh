9854ac69b6d533ae5e4da525e9ee02853a2b984b
Bogdan Stancescu <bogdan@moongate.ro>
2011-05-06 18:41:51 +0200

    No more hardcoded paths

--- gendoc.sh
+++ gendoc.sh
@@ -1,2 +1,7 @@
-rm -rf /var/www/html/projects.moongate.ro/octave-daemon/* &&
-phpdoc -o HTML:Smarty:PHP -q -i /examples/*,/test/*,/install/install-* -d . -t /var/www/html/projects.moongate.ro/octave-daemon -ti "Octave Daemon"
+TARGET="generated-documentation"
+if [ ! -d $TARGET ]; then mkdir $TARGET || {
+	echo Failed creating directory for documentation [$TARGET]
+	exit 1
+}; fi
+rm -rf $TARGET/* &&
+phpdoc -o HTML:Smarty:PHP -q -i /examples/*,/test/*,/install/install-* -d . -t $TARGET -ti "Octave Daemon"
