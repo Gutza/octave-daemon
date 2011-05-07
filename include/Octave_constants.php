5d7f248d640a404710fe03981bc672dbb9eb7800
Bogdan Stancescu <bogdan@moongate.ro>
2011-05-07 22:35:42 +0200

    Created

---
+++ include/Octave_constants.php
@@ -0,0 +1,39 @@
+<?php
+/**
+* Octave-daemon -- a network daemon for Octave, written in PHP
+*
+* This file stores the Octave-daemon constants.
+*
+* Copyright (C) 2011 Bogdan Stancescu <bogdan@moongate.ro>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see {@link http://www.gnu.org/licenses/}.
+*
+* @package octave-daemon
+* @subpackage common
+* @author Bogdan Stăncescu <bogdan@moongate.ro>
+* @version 1.0
+* @copyright Copyright (c) 2011, Bogdan Stăncescu
+* @license http://www.gnu.org/licenses/agpl-3.0.html GNU Affero GPL
+*/
+
+/**
+* This is the current Octave-daemon version.
+*/
+define("OCTAVE_DAEMON_VERSION","0.1.1 alpha");
+
+/**
+* This is the default port used by Octave-daemon
+*/
+define("OCTAVE_DAEMON_PORT",43210);
+
